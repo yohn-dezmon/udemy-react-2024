32e06a0e1e094dd84e6de77309ad9ad9a4e63e90
John Desmond <johndesmond631@gmail.com>
2025-01-19 14:54:54 -0500

    1-19 first half

--- projects/books/src/App.js
+++ projects/books/src/App.js
@@ -1,19 +1,21 @@
-import { useState } from 'react';
-import BookCreate from './components/BookCreate';
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
 
 function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
     console.log("Need to add book with: ", title);
-    const book = {id: 1, title: title};
+    const book = { id: 1, title: title };
     // how to append to a state list in react?
     setBooks([...books, book]);
-  }
+  };
 
-  return (<div>
-  <BookCreate />
-  </div>);
+  return (
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
